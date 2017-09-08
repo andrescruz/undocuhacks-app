17b235cfad4872ac27b23af82739feba26072f08
Andres Cruz <ac@andrescruz.net>
2017-09-08 00:48:49 -0700

    open Facebook and twitter

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 @Component({
   selector: 'page-contact',
@@ -7,12 +8,20 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private iab: InAppBrowser) {
 
   }
 
   emailUs() {
-    window.location.href = 'mailto:undocuhacks2017@undocumedia.org';
+    this.iab.create('mailto:undocuhacks2017@undocumedia.org');
+  }
+
+  twitter() {
+    this.iab.create('http://twitter.com/undocumedia');
+  }
+
+  facebook() {
+    this.iab.create('http://facebook.com/undocumedia');
   }
 
 }
